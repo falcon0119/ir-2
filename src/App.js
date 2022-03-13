756531e7b3ac86e2d580195af95598d7a179b37a
sockeye119 <58209598+sockeye119@users.noreply.github.com>
2022-03-13 18:37:09 +0900

    tsukuttayo

--- src/App.js
+++ src/App.js
@@ -6,46 +6,59 @@ import { useEffect, useState } from 'react';
 function App() {
   fetchIrInfo();
   const LatestIRImages = [
-    {img: "https://picsum.photos/500/300?random=1", href: ""},
-    {img: "https://picsum.photos/500/300?random=2", href: ""},
-    {img: "https://picsum.photos/500/300?random=3", href: ""},
-    {img: "https://picsum.photos/500/300?random=4", href: ""},
-    {img: "https://picsum.photos/500/300?random=5", href: ""},
+    { img: "https://picsum.photos/500/300?random=1", href: "" },
+    { img: "https://picsum.photos/500/300?random=2", href: "" },
+    { img: "https://picsum.photos/500/300?random=3", href: "" },
+    { img: "https://picsum.photos/500/300?random=4", href: "" },
+    { img: "https://picsum.photos/500/300?random=5", href: "" },
   ];
-  const [irinfo, setIrInfo]=useState([]);
-  useEffect(()=>{
-    fetchIrInfo().then( (data)=>{
-      const hage=data;
-      console.log({hage});
-      const hage2 = hage.map((e) => {
-        return {img: e.ImgUrl, href: e.URL }
+  const [irinfo, setIrInfo] = useState([]);
+  useEffect(() => {
+    fetchIrInfo().then(async (data) => {
+      const jsonData = await data.json()
+      const hage2 = jsonData.map((e) => {
+        return { img: e.ImgUrl, href: e.URL }
       })
-      console.log({hage2})
+      console.log({ hage2 })
       setIrInfo(hage2)
     })
   }, [])
+
+  const [text, setText] = useState('')
+  const handleChange = (e) => {
+    setText(() => e.target.value)
+  }
+
+  // const btnClick
   console.log(irinfo)
   return (
     <div className="App">
       <div className='Header'>
         <h1>IR 資料館</h1>
-          <h2>IR資料館は、日本企業の投資家向け資料を
-            検索できるサイトです。</h2>
+        <h2>IR資料館は、日本企業の投資家向け資料を
+          検索できるサイトです。</h2>
 
-            <div>
-              <input id="search_bar" type="text" placeholder='企業名を検索してください'></input>        
-            </div>
-            <div>
-              <input id="search_btn" type="button" value="検索" ></input> 
-            </div>
-            
+        <div>
+          <input id="search_bar" type="text" value={text} onChange={handleChange} placeholder='企業名を検索してください'></input>
+        </div>
+        <div>
+          <button id="search_btn" onClick={() => fetchIrInfo(text).then(async (data) => {
+            const jsonData = await data.json()
+            const hage2 = jsonData.map((e) => {
+              return { img: e.ImgUrl, href: e.URL }
+            })
+            console.log({ hage2 })
+            setIrInfo(hage2)
+          })} >検索</button>
+        </div>
+
       </div>
 
-      <div className='Latest' style={{width: "100%", backgroundColor: "green", margin: 20}}>
-        <div className='' style={{display: "flex", marginRight: "auto"}}>最新</div>
-        <div className='' style={{display: "flex", overflow: "auto"}} >
+      <div className='Latest' style={{ width: "100%", backgroundColor: "white", margin: 20 }}>
+        <div className='' style={{ display: "flex", marginRight: "auto" }}>最新</div>
+        <div className='' style={{ display: "flex", overflow: "auto" }} >
           {
-             irinfo.map((e) => (
+            irinfo.map((e) => (
               <IRImage href={e.href} src={e.img} />
             ))
           }
@@ -58,25 +71,15 @@ function App() {
 
 export default App;
 
-async function fetchIrInfo(){
-  return fetch('https://nm422zt9ul.execute-api.us-east-1.amazonaws.com/default/search_ir_info', {
-    method: "GET",
-    
+async function fetchIrInfo(search_term = "") {
+  const url = 'https://nm422zt9ul.execute-api.us-east-1.amazonaws.com/default/search_ir_info?search=' + search_term
+  return fetch(url, {
+    method: "GET"
   })
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    // 404 や 500 ステータスならここに到達する
-    throw new Error('Network response was not ok.');
-  })
-  .then(resJson => {
-    return resJson;
-  })
-  .catch(error => {
-    // ネットワークエラーの場合はここに到達する
-    console.error(error);
-  })
+    .catch(error => {
+      // ネットワークエラーの場合はここに到達する
+      console.error(error);
+    })
 }
 
 
